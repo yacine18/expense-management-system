fc400f0b95780c8c9c922ba8014e37a47656cb8a
yacine18 <hilali.yacin@gmail.com>
2021-09-26 21:26:13 +0100

    update-migration-folder

--- migrations/20210926154432-create-transaction.ts
+++ migrations/20210926154432-create-transaction.ts
@@ -1,6 +1,6 @@
 'use strict';
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
+  up: async (queryInterface:any, Sequelize:any) => {
     await queryInterface.createTable('Transactions', {
       id: {
         allowNull: false,
@@ -24,7 +24,7 @@ module.exports = {
       }
     });
   },
-  down: async (queryInterface, Sequelize) => {
+  down: async (queryInterface:any, Sequelize:any) => {
     await queryInterface.dropTable('Transactions');
   }
 };
--- migrations/20210926163847-create-user.ts
+++ migrations/20210926163847-create-user.ts
@@ -1,6 +1,6 @@
 'use strict';
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
+  up: async (queryInterface:any, Sequelize:any) => {
     await queryInterface.createTable('Users', {
       id: {
         allowNull: false,
@@ -27,7 +27,7 @@ module.exports = {
       }
     });
   },
-  down: async (queryInterface, Sequelize) => {
+  down: async (queryInterface:any, Sequelize:any) => {
     await queryInterface.dropTable('Users');
   }
 };
