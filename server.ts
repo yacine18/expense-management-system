b98b8b177d3189ad67398b51c2a5f4277b3ff3d0
yacine18 <hilali.yacin@gmail.com>
2021-10-07 23:11:44 +0100

    deploy-to-heroku

--- server.ts
+++ server.ts
@@ -22,7 +22,7 @@ app.use("/api/transactions", transactionRouter);
 app.use(express.static(path.join(__dirname, '/client/build')))
 app.get('*', (req,res)=> res.sendFile(path.join(__dirname, '/client/build/index.html')))
 
-const PORT = process.env.PORT || 8081;
+const PORT = "8081";
 db.sequelize.sync().then(() => {
     app.listen(PORT, () => {
         console.log(`App listening on port ${PORT}`)
