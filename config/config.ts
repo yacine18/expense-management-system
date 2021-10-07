7ddeece820f999b505848428b503329137f907b6
yacine18 <hilali.yacin@gmail.com>
2021-10-07 22:19:31 +0100

    deploy-to-heroku

--- config/config.ts
+++ config/config.ts
@@ -9,17 +9,17 @@ module.exports = {
     "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "username": "sql4442607",
+    "password": "IKlAMYLnPt",
+    "database": "sql4442607",
+    "host": "sql4.freesqldatabase.com",
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": "sql4442607",
+    "password": "IKlAMYLnPt",
+    "database": "sql4442607",
+    "host": "sql4.freesqldatabase.com",
     "dialect": "mysql"
   }
 }
