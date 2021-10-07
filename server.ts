56729a3193811ec8ec8c24873ad406001dd50e30
yacine18 <hilali.yacin@gmail.com>
2021-10-07 22:13:27 +0100

    deploy-to-heroku

--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
-import dotenv from 'dotenv'
-dotenv.config()
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+  }
 import express from "express";
 import cors from "cors";
 import db from "./models";
