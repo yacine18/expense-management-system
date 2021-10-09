ebe293bd8e0159b8202c28c7aa8b40392486ba48
yacine18 <hilali.yacin@gmail.com>
2021-10-09 14:05:04 +0100

    deploy-to-heroku

--- config/config.ts
+++ config/config.ts
@@ -2,24 +2,24 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME || "sql4442607",
+    password: process.env.DB_PASSWORD || "IKlAMYLnPt",
+    database: process.env.DB_DATABASE || "sql4442607",
+    host: process.env.DB_HOST || "sql4.freesqldatabase.com",
     dialect: "mysql",
   },
   test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME || "sql4442607",
+    password: process.env.DB_PASSWORD || "IKlAMYLnPt",
+    database: process.env.DB_DATABASE || "sql4442607",
+    host: process.env.DB_HOST || "sql4.freesqldatabase.com",
     dialect: "mysql",
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE ,
-    host: process.env.DB_HOST ,
+    username: process.env.DB_USERNAME || "sql4442607",
+    password: process.env.DB_PASSWORD || "IKlAMYLnPt",
+    database: process.env.DB_DATABASE || "sql4442607",
+    host: process.env.DB_HOST || "sql4.freesqldatabase.com",
     dialect: "mysql",
   },
 };
